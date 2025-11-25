851e144dd893bdb8ec200cf685a591ee8ffb5b31
IbtissamEchchaibi19 <ibtissam.echchaibi@gmail.com>
2025-11-25 15:19:42 +0100

    solve css errrors

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import '@syncfusion/ej2/material.css';
 import './index.css';
-import "@syncfusion/ej2-base/styles/material.css";
-import "@syncfusion/ej2-buttons/styles/material.css";
-import "@syncfusion/ej2-calendars/styles/material.css";
-import "@syncfusion/ej2-dropdowns/styles/material.css";
-import "@syncfusion/ej2-inputs/styles/material.css";
-import "@syncfusion/ej2-navigations/styles/material.css";
-import "@syncfusion/ej2-popups/styles/material.css";
-import "@syncfusion/ej2-splitbuttons/styles/material.css";
-import "@syncfusion/ej2-react-grids/styles/material.css";
+
 
 
 // Register Syncfusion license (you'll need to get a free trial license from syncfusion.com)
